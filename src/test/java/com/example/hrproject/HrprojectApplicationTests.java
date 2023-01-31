412776dd46dea9475e8f733c4645a72212c9ffe7
Zongdi <690160142@qq.com>
2023-01-31 20:18:35 +0800

    add dependency

---
+++ src/test/java/com/example/hrproject/HrprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
